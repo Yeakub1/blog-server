4cea446073602e8f6b5c42c1e0f57860a4f342b8
Yeakub1 <mryeakub10@gmail.com>
2024-06-15 23:45:50 +0600

    context

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { PrismaClient } from "@prisma/client";
 import { resolvers } from "./resolvers";
+import { typeDefs } from "./schema";
+import { jwtHelper } from "./utils/jwtHelpers";
+import { Context } from "./utils/types";
+
+export const prisma = new PrismaClient();
 
 const main = async () => {
   const server = new ApolloServer({
@@ -11,6 +16,15 @@ const main = async () => {
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async ({ req }): Promise<Context> => {
+      const userInfo = await jwtHelper.getUserInfoFromToken(
+        req.headers.authorization as string
+      );
+      return {
+        prisma,
+        userInfo,
+      };
+    },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,17 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
 export interface UserInfo {
   name: string;
   email: string;
   password: string;
   bio?: string;
+}
+
+
+export interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+  userInfo: {
+    userId: number | null;
+  } | null;
 }
