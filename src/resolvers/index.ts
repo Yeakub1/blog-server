262ce762f70f138b7b7dd8fc1d06224b8fe38a97
Yeakub1 <mryeakub10@gmail.com>
2024-06-15 23:27:34 +0600

    user info

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,62 +1,7 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { prisma } from "../utils/prisma";
-
-interface UserInfo {
-  name: string;
-  email: string;
-  password: string;
-}
+import { Query } from "./Query/Query";
+import { Mutation } from "./Mutation/Mutation";
 
 export const resolvers = {
-  Query: {
-    users: async (parent: any, args: any, context: any) => {
-      return await prisma.user.findMany();
-    },
-  },
-  Mutation: {
-    signup: async (parent: any, args: UserInfo, context: any) => {
-      const hashedPassword = await bcrypt.hash(args.password, 12);
-
-      const newUser = await prisma.user.create({
-        data: {
-          name: args.name,
-          email: args.email,
-          password: hashedPassword,
-        },
-      });
-
-      const token = jwt.sign({ userId: newUser.id }, "signature", {
-        expiresIn: "1d",
-      });
-      return {
-        userError: null,
-        token,
-      };
-    },
-    signin: async (parent: any, args: any, context: any) => {
-      const user = await prisma.user.findFirst({
-        where: {
-          email: args.email,
-        },
-      });
-
-      if (!user) {
-        throw new Error("User not found");
-      }
-      const valid = await bcrypt.compare(args.password, user.password);
-      if (!valid) {
-        throw new Error("Invalid password");
-      }
-
-      const token = jwt.sign({ userId: user.id }, "signature", {
-        expiresIn: "1d",
-      });
-
-      return {
-        userError: null,
-        token,
-      };
-    },
-  },
+  Query,
+  Mutation,
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,52 +1,66 @@
 export const typeDefs = `#graphql
+    type Query {
+        me: User
+        users: [User]
+        posts: [Post]
+        profile(userId: ID!): Profile
+    }
 
-   
-type Query {
-    me: User
-    users: [User]
-    posts: [Post]
-}
-    
-type Mutation {
-    signup(
-        name: String!,
-        email: String!,
-        password: String!,
-        bio: String
-    ): AuthPayload,
+    type Mutation {
+        signup(
+            name: String!,
+            email: String!,
+            password: String!
+            bio: String
+        ): AuthPayload,
+        
+        signin(
+            email: String!
+            password: String!
+        ): AuthPayload,
 
-    signin(
-        email: String!,
-        password: String!
-    ): AuthPayload
-}
+        addPost(post: PostInput!): PostPayload,
+        updatePost(postId: ID!, post: PostInput!): PostPayload,
+        deletePost(postId: ID!): PostPayload,
+        publishPost(postId: ID!): PostPayload
+    }
 
-type Post {
-    id: ID!,
-    title: String!,
-    content: String!,
-    author: User,
-    createdAt: String!,
-    published: Boolean!
-}
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        author: User
+        createdAt: String!
+        published: Boolean!
+    }
 
-type User {
-    id: ID!,
-    name: String!,
-    email: String!,
-    createdAt: String!,
-    posts: [Post]
-}
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        createdAt: String!
+        posts: [Post]
+    }
 
-type Profile {
-    id: ID!
-    bio: String!
-    createdAt: String!
-}
+    type Profile {
+        id: ID!
+        bio: String!
+        createdAt: String!
+        user: User!
+    }
 
-type AuthPayload {
-    userError: String
-    token: String
-}
- 
+    type AuthPayload {
+        userError: String
+        token: String
+    }
+
+    type PostPayload {
+        userError: String
+        post: Post
+    }
+
+    input PostInput {
+        title: String
+        content: String
+    }
 `;
