56cd545a97b74a7938957fc9f288f9876031f4a9
Yeakub1 <mryeakub10@gmail.com>
2024-06-18 14:10:52 +0600

    user with post

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { Query } from "./Query/Query";
 import { Mutation } from "./Mutation/Mutation";
 import { Post } from "./post";
+import { User } from "./Query/user";
 
 export const resolvers = {
   Query,
   Post,
+  User,
   Mutation,
 };
